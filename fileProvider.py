a2cceb7f4a8d4e559184ea6328defc6ab2acda86
Nikita Koval <nikita-g-koval@yandex.ru>
2023-12-17 22:51:55 +0300

    изменил тест для трёх типов вопросов

--- fileProvider.py
+++ fileProvider.py
@@ -1,4 +1,9 @@
 from user import User
+from question import Question
+from question_radioButton import QuestionRadioButton
+from question_checkButton import QuestionCheckButton
+from question_type import QuestionType
+from answer import Answer
 import os
 import json
 
@@ -65,7 +70,38 @@ class FileProvider:
         with open(FileProvider.questionsFileName, 'r', encoding='utf-8') as f:
             json_data = json.load(f)
 
-        return json_data
+        questions = []
+
+        for question in json_data['questions']:
+            answers = []
+            for answer in question['answers']:
+                answers.append(Answer(
+                    text=answer['text'],
+                    is_correct=answer['is_correct']
+                ))
+
+            question_type: QuestionType = question['type']
+
+            match question_type:
+                case QuestionType.base:
+                    current_question = Question(
+                        text=question['text'],
+                        answers=answers
+                    )
+                case QuestionType.radio_button:
+                    current_question = QuestionRadioButton(
+                        text=question['text'],
+                        answers=answers
+                    )
+                case QuestionType.check_button:
+                    current_question = QuestionCheckButton(
+                        text=question['text'],
+                        answers=answers
+                    )
+
+            questions.append(current_question)
+
+        return questions
 
 
 def class_to_dict(obj):
--- test_window.py
+++ test_window.py
@@ -1,6 +1,11 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+from typing import List
+from question_type import QuestionType
+from question import Question
+from question_radioButton import QuestionRadioButton
+from question_checkButton import QuestionCheckButton
 from answer import Answer
 from user import User
 from questionsStrorage import QuestionsStorage
@@ -18,6 +23,8 @@ position = {'anchor': S}
 class TestWindow:
     def __init__(self, user: User, questions_storage: QuestionsStorage):
         self.user = user
+        self.user.rightAnswersCount = 0
+
         self.questions_storage = questions_storage
         self.test_window = Tk()
         self.test_window.title("Тест")
@@ -31,30 +38,71 @@ class TestWindow:
         self.questions = self.qs.shuffle(questions)
         self.questionId = 0
 
-        answers = self.questions[self.questionId].answers
-        self.answers = self.qs.shuffle(answers)
+        self.shuffled_answers = self.qs.shuffle(self.current_answers)
 
-        self.test_label = Label(self.test_window, text="Вопрос №{0}:".format(self.questionId + 1)
-                                                       + self.questions[self.questionId].text, font=font_entry,
+        self.test_label = Label(self.test_window, font=font_entry,
                                 justify=CENTER, **header_padding)
         self.test_label.pack()
 
         self.selected_id = IntVar(self.test_window)
-
+        self.answer_entry = Entry(self.test_window)
         self.radio_buttons = []
-
-        self.init_radiobuttons()
+        self.check_buttons = []
+        self.selected_buttons = []
 
         self.acceptAnswer_btn = Button(self.test_window, text='Ответить', command=self.accept_answer_btn_clicked)
         self.acceptAnswer_btn.pack(pady=10, side=BOTTOM)
 
-    def get_answer(self):
-        answer: Answer = self.answers[self.selected_id.get()]
+        self.show_next_question()
+
+    @property
+    def current_question(self):
+        return self.questions[self.questionId]
+
+    @property
+    def current_answers(self):
+        return self.current_question.answers
+
+    def show_answers(self):
+        self.clear_answers()
+
+        match self.current_question.get_type:
+            case QuestionType.base:
+                self.answer_entry = Entry(self.test_window, bg='#fff', fg='#444', font=font_entry)
+                self.answer_entry.pack(**base_padding)
+            case QuestionType.radio_button:
+                self.init_radiobuttons()
+            case QuestionType.check_button:
+                self.init_checkbuttons()
+
+    def get_answer_from_radioButton(self):
+        answer: Answer = self.shuffled_answers[self.selected_id.get()]
         return answer
 
     def accept_answer_btn_clicked(self):
-        if self.get_answer().is_correct:
-            self.user.rightAnswersCount += 1
+        match self.current_question.get_type:
+            case QuestionType.base:
+                if self.answer_entry.get() == self.current_answers[0].text:
+                    self.user.rightAnswersCount += 1
+            case QuestionType.radio_button:
+                if self.get_answer_from_radioButton().is_correct:
+                    self.user.rightAnswersCount += 1
+            case QuestionType.check_button:
+                attempts = 0
+                successful_attempts = 0
+                for btn in self.selected_buttons:
+                    if btn.get() > len(self.shuffled_answers):
+                        continue
+                    attempts += 1
+
+                    if self.shuffled_answers[btn.get()].is_correct:
+                        successful_attempts += 1
+
+                successful_percentage = successful_attempts/attempts * 100
+
+                if successful_percentage > 50:
+                    self.user.rightAnswersCount += 1
+
         if self.questionId >= len(self.questions)-1:
             diagnosis = self.diagnoses.calculate_diagnose(len(self.questions), self.user.rightAnswersCount)
             messagebox.showinfo(title="Тест завершён", message="{0}, ваш диагноз: {1}".format(self.user.name,
@@ -64,23 +112,43 @@ class TestWindow:
         self.show_next_question()
 
     def show_next_question(self):
-        self.test_label.config(text="Вопрос №{0}:".format(self.questionId+1) + self.questions[self.questionId].text)
-        self.clear_radiobuttons()
-        self.init_radiobuttons()
+        self.test_label.config(text="Вопрос №{0}:".format(self.questionId+1) + self.current_question.text)
+        self.show_answers()
 
     def init_radiobuttons(self):
-        answers = self.questions[self.questionId].answers
-        self.answers = self.qs.shuffle(answers)
+        self.shuffled_answers = self.qs.shuffle(self.current_answers)
 
         self.selected_id.set(0)
 
-        for i in range(len(self.answers)):
-            answer_btn = ttk.Radiobutton(self.test_window, text=self.answers[i].text, value=i,
+        for i in range(len(self.shuffled_answers)):
+            answer_btn = ttk.Radiobutton(self.test_window, text=self.shuffled_answers[i].text, value=i,
                                          variable=self.selected_id)
             answer_btn.pack(**base_padding)
             self.radio_buttons.append(answer_btn)
 
-    def clear_radiobuttons(self):
+    def init_checkbuttons(self):
+        self.shuffled_answers = self.qs.shuffle(self.current_answers)
+
+        for i in range(len(self.shuffled_answers)):
+            selected_id = IntVar(self.test_window)
+            selected_id.set(len(self.shuffled_answers) + 1)
+            self.selected_buttons.append(selected_id)
+
+            answer_btn = ttk.Checkbutton(self.test_window, text=self.shuffled_answers[i].text,
+                                         offvalue=len(self.shuffled_answers) + 1,
+                                         onvalue=i,
+                                         variable=selected_id)
+            answer_btn.pack(**base_padding)
+            self.check_buttons.append(answer_btn)
+
+    def clear_answers(self):
+        self.answer_entry.destroy()
         for btn in self.radio_buttons:
             btn.destroy()
+
+        for btn in self.check_buttons:
+            btn.destroy()
+
         self.radio_buttons.clear()
+        self.check_buttons.clear()
+        self.selected_buttons.clear()
